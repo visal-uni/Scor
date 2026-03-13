bf28ab9f1c189c3e18b5ce6d851e239fd77193dd
visal <salkizzyou@gmail.com>
2026-03-14 00:51:16 +0700

    api

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -1,19 +1,32 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET ?? process.env.JWT_SECRET;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET ?? process.env.JWT_SECRET;
+
+const ACCESS_TOKEN_EXPIRES_IN = "15m";
+const REFRESH_TOKEN_EXPIRES_IN = "7d";
 
 export const generateAccessToken = (userId) => {
-    return jwt.sign({sub: userId}, process.env.JWT_SECRET_ACCESSTOKEN, {expiresIn: "15m"});
-}
+  return jwt.sign(
+    { sub: userId },
+    ACCESS_TOKEN_SECRET,
+    { expiresIn: ACCESS_TOKEN_EXPIRES_IN }
+  );
+};
 
 export const generateRefreshToken = (userId) => {
-    return jwt.sign({sub: userId}, process.env.JWT_SECRET_REFRESHTOKEN, {expiresIn: "7d"});
-}
+  return jwt.sign(
+    { sub: userId },
+    REFRESH_TOKEN_SECRET,
+    { expiresIn: REFRESH_TOKEN_EXPIRES_IN }
+  );
+};
 
 export const verifyAccessToken = (token) => {
-    return jwt.verify(token, process.env.JWT_SECRET_ACCESSTOKEN);
-}
+  return jwt.verify(token, ACCESS_TOKEN_SECRET);
+};
 
 export const verifyRefreshToken = (token) => {
-    return jwt.verify(token, process.env.JWT_SECRET_REFRESHTOKEN);
-}
+  return jwt.verify(token, REFRESH_TOKEN_SECRET);
+};
+
